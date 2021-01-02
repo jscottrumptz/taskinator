03f43b056c302dfe956ce75a482d92ce02c00386
J Scott Rumptz <jscott@rumptz.tech>
2021-01-01 18:40:00 -0600

    now captures form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+let createTaskHandler = function(event) {
     event.preventDefault();
-  
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add list item
+    let taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list itme to list
     tasksToDoEl.appendChild(listItemEl);
   };
 
